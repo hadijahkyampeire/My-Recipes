61a38e8e3edabec6b1f1928b712fb6082b252615
hadijahkyampeire <hadijah315@gmail.com>
2018-04-11 10:36:45 +0300

    implements mapping

--- src/components/categories/dashboard.js
+++ src/components/categories/dashboard.js
@@ -4,10 +4,20 @@ import React, { Component } from 'react';
 import CreateCategory from './CreateCategories';
 import { FetchCategoriesAction } from '../../Action_creators/categoryAction';
 
-const Category = (props)=>(
-  _.map(category => {
-    <div className="dashboard" style={{marginLeft:20, paddingTop:10}}>
-    <div className="card">
+class Dashboard extends Component{
+  componentDidMount() {
+    this.props.FetchCategoriesAction();
+}
+
+    render(){
+
+      const { categories } = this.props.categories;
+        return _.map(categories, category => {
+          return(
+            <div>
+            <CreateCategory />
+            <div className="dashboard" style={{marginLeft:20, paddingTop:10}}>
+            <div className="card">
     <div className="card-image waves-effect waves-block waves-light">
       <img className="activator" alt="" src={"https://images.unsplash.com/photo-1480348709911-be15f2c579ff?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=eec72ade9455b8514a3431fbe7311e46&auto=format&fit=crop&w=800&q=60"}/>
     </div>
@@ -19,29 +29,15 @@ const Category = (props)=>(
       <span className="card-title grey-text text-darken-4">Card Title<i className="material-icons right">close</i></span>
       <p>Here is some more information about this product that is only revealed once clicked on.</p>
     </div>
+  </div>
         </div> 
-</div>
-
-}
-  
-))
-
-class Dashboard extends Component{
-  componentDidMount() {
-    this.props.FetchCategoriesAction();
-}
-
-    render(){
-      const { categories } = this.props.categories;
-        return (
-          <div> 
-            <CreateCategory />
-            <Category categories={categories}/>
-          </div>
-          );
+        </div>
+        );
+    });
 }}
 
 const mapStateToProps = (state) => {
+    console.log(state)
     return { 
         categories: state.categories
     }
